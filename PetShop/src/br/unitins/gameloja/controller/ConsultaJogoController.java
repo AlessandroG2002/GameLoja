931bc24d38ecf4b8acdcb76b5dabd2995f8cdd25
AlessandroG2002 <alessandrogabrielaula@gmail.com>
2021-06-05 01:09:46 -0300

    Projeto funcional, atividade A2 - 1

--- PetShop/src/br/unitins/gameloja/controller/ConsultaJogoController.java
+++ PetShop/src/br/unitins/gameloja/controller/ConsultaJogoController.java
@@ -9,12 +9,9 @@ import javax.faces.context.Flash;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 
-import br.unitins.gameloja.application.Util;
 import br.unitins.gameloja.dao.DAO;
 import br.unitins.gameloja.dao.JogoDAO;
-import br.unitins.gameloja.model.Peso;
 import br.unitins.gameloja.model.Jogo;
-import br.unitins.gameloja.model.TipoPeso;
 
 @Named
 @ViewScoped
